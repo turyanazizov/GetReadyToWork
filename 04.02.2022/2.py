f27864f91dd6007935d6832a430bcfb32e26dd10
Turyan Azizov <turyanazizov13@gmail.com>
2022-02-04 17:27:10 +0400

    2nd exercise

---
+++ 04.02.2022/2.py
@@ -0,0 +1,10 @@
+person = {
+    'first_name': 'Turyan',
+    'last_name': 'Azizov',
+    'age': 21,
+    'favorite_colors': ['blue', 'green'],
+    'country': 'Azerbaijan'
+}
+print('All keys: ',person.keys())
+print('All values: ',person.values())
+print('All items: ',person.items())
