c31f016afff2de0e2e410e30c1543764c68bd014
Turyan Azizov <turyanazizov13@gmail.com>
2022-02-04 17:18:10 +0400

    1st exercise

---
+++ 04.02.2022/1.py
@@ -0,0 +1,20 @@
+# Example for List
+thislist = ["apple", 212, "cherry",True,[2,4,6]]
+print(thislist)
+
+# Example for Tuple
+thistuple = ("apple", "banana", 21, True, False,[3,46],(32,353))
+print(thistuple[5][1])
+
+# Example for Dictionary
+thisdict = {
+  "name": "Turyan",
+  "surname": "Azizov",
+  "country":"Azerbaijan",
+  "Age": 21
+}
+print(thisdict["surname"])
+
+# Example for Set
+thisset = {"alim", 34, True, 240, "male",True}
+print(240 in thisset)
